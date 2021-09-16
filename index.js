41e69ba5a52c16060c012e79f81c933ba95ee70b
Sheikh Abdun <san06190@gmail.com>
2021-09-16 17:37:17 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,67 @@
+var numBtn = document.querySelectorAll("button").length;
+var i = 0;
+while (i < numBtn) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+   
+   var kp = this.innerHTML;
+   makesound(kp);
+   btnAnimation(kp); 
+  });
+  i++;
+}
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+   btnAnimation(event.key); 
+
+})
+function makesound(key)
+{
+    switch (key) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+  
+        case "a":
+          var audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+  
+        case "s":
+          var audio = new Audio("sounds/tom-3.mp3");
+          audio.play();
+          break;
+  
+        case "d":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+          break;
+  
+        case "j":
+          var audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+  
+        case "k":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+  
+        case "l":
+          var audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+  
+        default:
+            console.log("sorry");
+          break;
+      }
+}
+function btnAnimation(bkey){
+    var b = document.querySelector("."+ bkey);
+    b.classList.add("pressed");
+    setTimeout(() => {
+       b.classList.remove("pressed"); 
+    }, 100);
+
+}
